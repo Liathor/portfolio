e3de2284ccd7a66678fe3a1c09fbed241bd00037
Liathor <dyermisha@gmail.com>
2025-03-11 22:25:44 -0400

    Moved local images files and updated links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
       <footer className="footer">
-        <a href="https://www.linkedin.com/in/misha-dyer-042703101/"><img className="footerLink" src="./src/assets/LI-In-Bug.png" alt="LinkedIn" /></a>
-        <a href="https://github.com/Liathor"><img className="footerLink" src="./src/assets/github-mark.png" alt="GitHub" /></a>
-        <a href="https://www.facebook.com/misha.dyer"><img className="footerLink" src="./src/assets/Facebook_logo_Primary.png" alt="Facebook" /></a>
+        <a href="https://www.linkedin.com/in/misha-dyer-042703101/"><img className="footerLink" src="/images/LI-In-Bug.png" alt="LinkedIn" /></a>
+        <a href="https://github.com/Liathor"><img className="footerLink" src="/images/github-mark.png" alt="GitHub" /></a>
+        <a href="https://www.facebook.com/misha.dyer"><img className="footerLink" src="/images/Facebook_Logo_Primary.png" alt="Facebook" /></a>
       </footer>
     );
   }
