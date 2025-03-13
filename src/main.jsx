bd299f10e20cacd34edb28131b0b7862db2b470e
Liathor <dyermisha@gmail.com>
2025-03-13 18:17:15 -0400

    fixed missing index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import './App.css';
-
-import './index.css'
 import App from './App.jsx'
 import About from './pages/about';
 import Portfolio from './pages/portfolio';
